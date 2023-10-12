f67cf787e3950c7456170065d65a145f0540e17b
Eliran <eliranschool@gmail.com>
2023-10-12 18:30:17 +0300

    added forms and prisma backend

--- src/app/utils/types.ts
+++ src/app/utils/types.ts
@@ -11,8 +11,23 @@ export type PersonData = {
   notes: string;
   source: string;
 };
+
+export type AnimalData = {
+  id?: string;
+  imageUrl?: string;
+  name: string;
+  ownerName: string;
+  ownerPhoneNumber: string;
+  status: string; //"unknown" | "found" | "deceased" | "kidnapped";
+  lastSeen: string;
+  identifyingDetails: string;
+  notes: string;
+  source: string;
+};
+
 export type NotificationData = {
   email: string;
   notify_id: string;
 };
+
 export default PersonData;
